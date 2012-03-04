ed4f3d9b58177a22173fcded6a9c77aaf1237975
lin.hua.cheng2008@gmail.com <lin.hua.cheng2008@gmail.com@23e45407-9a27-f170-2efe-fffcf4b299b2>
2012-03-04 21:17:51 +0000

    Request pet switch code

---
+++ trunk/petsitter/src/main/java/com/sjsu/petsitter/util/RequestStatus.java
@@ -0,0 +1,9 @@
+package com.sjsu.petsitter.util;
+
+public interface RequestStatus {
+	
+	public static final String NEW = "New";
+	public static final String CONFIRMED = "Confirmed";
+	
+	//NEW, CONFIRMED;
+}
