441986d1e0fcdff0ccd7cc7624f50bf838164b36
lin.hua.cheng2008@gmail.com <lin.hua.cheng2008@gmail.com@23e45407-9a27-f170-2efe-fffcf4b299b2>
2012-03-06 09:07:12 +0000

    added code for request creation

--- trunk/petsitter/src/main/java/com/sjsu/petsitter/web/RequestController.java
+++ trunk/petsitter/src/main/java/com/sjsu/petsitter/web/RequestController.java
@@ -1,7 +1,10 @@
 package com.sjsu.petsitter.web;
 
 import javax.servlet.http.HttpServletRequest;
+
 import com.sjsu.petsitter.domain.Request;
+import com.sjsu.petsitter.domain.Address;
+
 import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +14,9 @@ import com.sjsu.petsitter.domain.User;
 import java.math.BigInteger;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.sjsu.petsitter.util.RequestStatus;
+import org.springframework.validation.BindingResult;
+import javax.validation.Valid;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @RequestMapping("/requests")
 @Controller
@@ -23,32 +29,71 @@ public class RequestController {
     @RequestMapping(params = "form", produces = "text/html")
     public String createForm(Model uiModel, HttpServletRequest httpServletRequest) {
     	
-    	// petSitterId
-    	String strPetSitterId = httpServletRequest.getParameter("petSitterId");
+    	// approverId
+    	String strApproverId = httpServletRequest.getParameter("approverId");
     	
-    	if (strPetSitterId == null || strPetSitterId.length() ==0) {
+    	if (strApproverId == null || strApproverId.length() ==0) {
     		return "requests/create";
     	}
     	
-    	BigInteger petSitterId = new BigInteger(strPetSitterId);
-    	System.out.println("PetSitterId:" + petSitterId);
+    	BigInteger approverId = new BigInteger(strApproverId);
     	
-    	User petSitter = userService.findUser(petSitterId);
+    	User approver = userService.findUser(approverId);
+    	Address approverAddress =  getAddress(approver);
+    	
+    	
+    	String requestorUserName = getLogonUsername();
+    	User requestor = userService.findPetOwnerByUsername(requestorUserName);
+    	Address requestorAddress = getAddress(requestor);
     	
     	Request request = new Request();
-    	request.setPetSitterId(petSitter.getId());
     	request.setStatus(RequestStatus.NEW);
-    	
-    	User myAccount = userService.findUser(new BigInteger("24549844967302108965912466479"));
+    	request.setRequestorId(requestor.getId().toString());
+    	request.setRequestorUserName(requestor.getUserName());
+    	request.setApproverUserName(approver.getUserName());
+    	request.setApproverId(approver.getId().toString());
     	
         uiModel.addAttribute("request", request);
-        uiModel.addAttribute("myPets", myAccount.getPets());
-        uiModel.addAttribute("petSitterName", petSitter.getDisplayName());
-        uiModel.addAttribute("petSitterPets", petSitter.getPets());
+        uiModel.addAttribute("requestorPets", requestor.getPets());
+        uiModel.addAttribute("approverName", approver.getDisplayName());
+        uiModel.addAttribute("approverPets", approver.getPets());
+        uiModel.addAttribute("requestorAddress", requestorAddress);
+        uiModel.addAttribute("approverAddress", approverAddress);
         addDateTimeFormatPatterns(uiModel);
         
         return "requests/create";
     }
+     
+    private Address getAddress (User user) {
+    	
+    	Address address = new Address();
+    	address.setLine1(user.getAddressLine1());
+    	address.setLine2(user.getAddressLine2());
+    	address.setCity(user.getCity());
+    	address.setZipCode(user.getZip());
+    	address.setHomePhone(user.getHomePhone());
+    	address.setMobile(user.getMobile());
+    	return address;
+    }
     
     
+	public static String getLogonUsername() {	
+	//	Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		//String username = ((UserDetails)principal).getUsername();
+
+	//	return username;
+		return "petowner1";
+	}
+	
+	 @RequestMapping(method = RequestMethod.POST, produces = "text/html")
+	 public String create(@Valid Request request, BindingResult bindingResult, Model uiModel, HttpServletRequest httpServletRequest) {
+	        if (bindingResult.hasErrors()) {
+	            populateEditForm(uiModel, request);
+	            return "requests/create";
+	        }
+	        uiModel.asMap().clear();
+	        requestService.saveRequest(request);
+	        return "redirect:/requests/" + encodeUrlPathSegment(request.getId().toString(), httpServletRequest);
+	    }
+
 }
