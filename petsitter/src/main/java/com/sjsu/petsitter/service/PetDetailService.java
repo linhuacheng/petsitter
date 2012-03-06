c6819a5963661ebc35fc5e692e903aa7728f9b45
lin.hua.cheng2008@gmail.com <lin.hua.cheng2008@gmail.com@23e45407-9a27-f170-2efe-fffcf4b299b2>
2012-03-06 09:03:11 +0000

    Added search by petId that hits the user table.

--- petsitter/src/main/java/com/sjsu/petsitter/service/PetDetailService.java
+++ petsitter/src/main/java/com/sjsu/petsitter/service/PetDetailService.java
@@ -2,6 +2,10 @@ package com.sjsu.petsitter.service;
 
 import org.springframework.roo.addon.layers.service.RooService;
 
+import com.sjsu.petsitter.domain.PetDetail;
+
 @RooService(domainTypes = { com.sjsu.petsitter.domain.PetDetail.class })
 public interface PetDetailService {
+	
+    public PetDetail findPetDetailByPetId(String petId);
 }
--- petsitter/src/main/java/com/sjsu/petsitter/service/PetDetailServiceImpl.java
+++ petsitter/src/main/java/com/sjsu/petsitter/service/PetDetailServiceImpl.java
@@ -1,5 +1,33 @@
 package com.sjsu.petsitter.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import com.sjsu.petsitter.domain.PetDetail;
+import com.sjsu.petsitter.domain.User;
+
 
 public class PetDetailServiceImpl implements PetDetailService {
+	
+	  @Autowired
+	  MongoTemplate mongoTemplate;
+	  
+    public PetDetail findPetDetailByPetId(String petId) {
+        Query query = new Query(Criteria.where("pets.petId").is(petId));
+
+//      return userRepository.findPetOwnerByPetType(searchRequestBean.getPetType()
+//              , new org.springframework.data.domain.PageRequest(firstResult / maxResults, maxResults));
+        User user = mongoTemplate.findOne(query, User.class);
+        PetDetail pet = null;
+        if (user != null && user.getPets()!=null) {
+	        for (PetDetail petDetail: user.getPets()) {
+	        	if (petDetail.getPetId().equals(petId)) {
+	        		pet =  petDetail;
+	        	}
+	        }
+        }
+        return pet;
+    }
 }
