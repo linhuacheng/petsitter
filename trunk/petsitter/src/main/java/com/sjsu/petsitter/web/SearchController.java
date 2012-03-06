3b518cc2ec1d676f42118e07b60e2f359143b87c
chandu.kempaiah@gmail.com <chandu.kempaiah@gmail.com@23e45407-9a27-f170-2efe-fffcf4b299b2>
2012-03-06 08:19:33 +0000

    Pet owner search feature -Show details of pet owner with pet details -Return back to search from details

--- trunk/petsitter/src/main/java/com/sjsu/petsitter/web/SearchController.java
+++ trunk/petsitter/src/main/java/com/sjsu/petsitter/web/SearchController.java
@@ -4,13 +4,20 @@ import com.sjsu.petsitter.bean.SearchRequestBean;
 import com.sjsu.petsitter.domain.PetDetail;
 import com.sjsu.petsitter.domain.User;
 import com.sjsu.petsitter.service.UserService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.roo.addon.json.RooJson;
 import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpSession;
+import java.math.BigInteger;
 
 /**
  * Created by IntelliJ IDEA.
@@ -23,7 +30,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/search")
 @RooWebScaffold(path = "search", formBackingObject = SearchRequestBean.class)
 public class SearchController {
-
+    Log log = LogFactory.getLog(SearchController.class);
     @Autowired
     UserService userService;
     @RequestMapping(params = "form", produces = "text/html")
@@ -33,6 +40,7 @@ public class SearchController {
     }
 
     void populateEditForm(Model uiModel, SearchRequestBean searchRequestBean) {
+
         uiModel.addAttribute("searchRequestBean", searchRequestBean);
         //addDateTimeFormatPatterns(uiModel);
     }
@@ -40,7 +48,7 @@ public class SearchController {
     public String searchPetOwner(SearchRequestBean searchRequestBean
             , @RequestParam(value = "page", required = false) Integer page
             , @RequestParam(value = "size", required = false) Integer size
-            , Model uiModel){
+            , Model uiModel, HttpSession session){
 
         int sizeNo = size == null ? 10 : size.intValue();
         final int firstResult = page == null ? 0 : (page.intValue() - 1) * sizeNo;
@@ -48,6 +56,16 @@ public class SearchController {
         float nrOfPages = (float) userService.countAllUsers() / sizeNo;
         uiModel.addAttribute("maxPages", (int) ((nrOfPages > (int) nrOfPages || nrOfPages == 0.0) ? nrOfPages + 1 : nrOfPages));
         populateEditForm(uiModel, searchRequestBean);
+        log.debug("SearchRequestBean:"+searchRequestBean);
+        log.debug("SearchRequestBean Session:"+session.getAttribute("searchRequestBean"));
+        session.setAttribute("searchRequestBean", searchRequestBean);
         return "search/searchWithResult";
+    }
+
+    @RequestMapping(value="/{id}", produces = "text/html")
+    public String getOwnerInfo(@PathVariable("id")BigInteger id, Model uiModel){
+       uiModel.addAttribute("user", userService.findUser(id));
+        uiModel.addAttribute("itemId", id);
+        return "search/show";
     }
 }
