95c14e177d31885bbf23a88fcbead3be50cb8541
nikitha.mamidi@gmail.com <nikitha.mamidi@gmail.com@23e45407-9a27-f170-2efe-fffcf4b299b2>
2012-03-07 06:56:16 +0000

    Methods for create, list, update, delete petdetails

--- trunk/petsitter/src/main/java/com/sjsu/petsitter/service/PetDetailServiceImpl.java
+++ trunk/petsitter/src/main/java/com/sjsu/petsitter/service/PetDetailServiceImpl.java
@@ -1,10 +1,19 @@
 package com.sjsu.petsitter.service;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import com.sjsu.petsitter.domain.PetDetail;
 import com.sjsu.petsitter.domain.User;
 
@@ -30,4 +39,88 @@ public class PetDetailServiceImpl implements PetDetailService {
         }
         return pet;
     }
+    
+public void savePetDetailToUser(PetDetail petdetail){
+    	
+    	Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		String username = ((UserDetails)principal).getUsername();
+		petdetail.setPetId(UUID.randomUUID().toString());
+		Set<PetDetail> pets = new HashSet<PetDetail>();
+		Query query = new Query(Criteria.where("userName").is(username));
+		User user = mongoTemplate.findOne(query, User.class);
+
+		if(user.getPets()!=null)
+		{
+			pets = user.getPets();
+			pets.add(petdetail);
+		}
+		else
+			pets.add(petdetail);
+		user.setPets(pets);
+		mongoTemplate.save(user);
+		
+    }
+    
+    public List<PetDetail> findPetDetailsByUser(){
+    	Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		String username = ((UserDetails)principal).getUsername();
+		
+		Set<PetDetail> pets = new HashSet<PetDetail>();
+		Query query = new Query(Criteria.where("userName").is(username));
+		User user = mongoTemplate.findOne(query, User.class);
+		if(user.getPets()!=null)
+		{
+			pets = user.getPets();
+			
+		}
+		
+		List<PetDetail> list = new ArrayList<PetDetail>(pets);
+		return list;
+		
+    }
+    
+    public void deletePetDetailByUser(String petId)
+    {
+    	Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		String username = ((UserDetails)principal).getUsername();
+		Set<PetDetail> pets = new HashSet<PetDetail>();
+		Query query = new Query(Criteria.where("userName").is(username));
+		User user = mongoTemplate.findOne(query, User.class);
+		if (user != null && user.getPets()!=null) {
+			pets = user.getPets();
+	        for (PetDetail petDetail: pets) {
+	        	if (petDetail.getPetId().equals(petId)) {
+	        		pets.remove(petDetail);
+	        		break;
+	        	}
+	        }
+	        user.setPets(pets);
+	        mongoTemplate.save(user);
+        }
+		
+    }
+    
+    public void updatePetIdByUser(PetDetail petdetail){
+    	
+    	Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		String username = ((UserDetails)principal).getUsername();
+		Set<PetDetail> pets = new HashSet<PetDetail>();
+		String petId = petdetail.getPetId();
+		PetDetail pet = new PetDetail();
+		Query query = new Query(Criteria.where("userName").is(username));
+		User user = mongoTemplate.findOne(query, User.class);
+		if (user != null && user.getPets()!=null) {
+			pets = user.getPets();
+	        for (PetDetail petDetail: pets) {
+	        	if (petDetail.getPetId().equals(petId)) {
+	        		pets.remove(petDetail);	
+	        		break;
+	        	}
+	        }
+	        pets.add(petdetail);
+	        user.setPets(pets);
+	        mongoTemplate.save(user);
+        }
+    }
+
 }
