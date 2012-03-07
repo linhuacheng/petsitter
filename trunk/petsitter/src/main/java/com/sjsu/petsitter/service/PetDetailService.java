6794e946c00c06655f0303e4017f584d784205a2
nikitha.mamidi@gmail.com <nikitha.mamidi@gmail.com@23e45407-9a27-f170-2efe-fffcf4b299b2>
2012-03-07 06:55:44 +0000

    Added methods for getting pet details by user

--- trunk/petsitter/src/main/java/com/sjsu/petsitter/service/PetDetailService.java
+++ trunk/petsitter/src/main/java/com/sjsu/petsitter/service/PetDetailService.java
@@ -1,5 +1,7 @@
 package com.sjsu.petsitter.service;
 
+import java.util.List;
+
 import org.springframework.roo.addon.layers.service.RooService;
 
 import com.sjsu.petsitter.domain.PetDetail;
@@ -8,4 +10,8 @@ import com.sjsu.petsitter.domain.PetDetail;
 public interface PetDetailService {
 	
     public PetDetail findPetDetailByPetId(String petId);
+    public void savePetDetailToUser(PetDetail petdetail);
+    public void deletePetDetailByUser(String petId);
+    public List<PetDetail> findPetDetailsByUser();
+    public void updatePetIdByUser(PetDetail petDetail);
 }
