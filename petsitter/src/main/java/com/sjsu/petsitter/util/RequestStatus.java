ac387ec9956791df0b981cf8dac617d33e7cc5ae
lin.hua.cheng2008@gmail.com <lin.hua.cheng2008@gmail.com@23e45407-9a27-f170-2efe-fffcf4b299b2>
2012-03-08 08:40:49 +0000

    Fixed response action buttons

--- petsitter/src/main/java/com/sjsu/petsitter/util/RequestStatus.java
+++ petsitter/src/main/java/com/sjsu/petsitter/util/RequestStatus.java
@@ -4,6 +4,9 @@ public interface RequestStatus {
 	
 	public static final String NEW = "New";
 	public static final String CONFIRMED = "Confirmed";
-	
+	public static final String ACCEPTED = "Accepted";
+	public static final String CLOSED = "Closed";
+	public static final String CANCELLED = "Cancelled";
+	public static final String REJECTED = "Rejected";
 	//NEW, CONFIRMED;
 }
