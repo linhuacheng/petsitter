8c7d0879d114487383fad84276d0bb42558ea288
lin.hua.cheng2008@gmail.com <lin.hua.cheng2008@gmail.com@23e45407-9a27-f170-2efe-fffcf4b299b2>
2012-04-30 03:12:18 +0000

    search pet owner checkin-in

---
+++ trunk/petsitter-android-client/src/com/android/petswitch/dto/PetOwnerResult.java
@@ -0,0 +1,143 @@
+package com.android.petswitch.dto;
+
+import java.math.BigInteger;
+
+public class PetOwnerResult {
+
+    private String displayName;
+    private BigInteger id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String addressLine1;
+    private String addressLine2;
+    private String city;
+    private String zip;
+    private String country;
+    private String homePhone;
+    private String mobile;
+    private Double averageRating;
+    private int latitude;
+    private int longitude;
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public BigInteger getId() {
+        return id;
+    }
+
+    public void setId(BigInteger id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    public String getAddressLine2() {
+        return addressLine2;
+    }
+
+    public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getHomePhone() {
+        return homePhone;
+    }
+
+    public void setHomePhone(String homePhone) {
+        this.homePhone = homePhone;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public Double getAverageRating() {
+        return averageRating;
+    }
+
+    public void setAverageRating(Double averageRating) {
+        this.averageRating = averageRating;
+    }
+    
+    public int getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(int latitude) {
+		this.latitude = latitude;
+	}
+
+	public int getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(int longitude) {
+		this.longitude = longitude;
+	}
+
+}
---
+++ trunk/petsitter-android-client/src/com/android/petswitch/dto/PetOwnerResultRest.java
@@ -0,0 +1,18 @@
+package com.android.petswitch.dto;
+
+import java.util.List;
+
+public class PetOwnerResultRest {
+
+	private List<PetOwnerResult> petOwners;
+
+	public List<PetOwnerResult> getPetOwners() {
+		return petOwners;
+	}
+
+	public void setPetOwners(List<PetOwnerResult> petOwners) {
+		this.petOwners = petOwners;
+	}
+	
+	
+}
