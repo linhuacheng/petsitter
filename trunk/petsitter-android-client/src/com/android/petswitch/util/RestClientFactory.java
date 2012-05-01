a405ba85c25da8329ebc785b93c56932319f6a56
nikitha.mamidi@gmail.com <nikitha.mamidi@gmail.com@23e45407-9a27-f170-2efe-fffcf4b299b2>
2012-05-01 22:08:10 +0000

    code related to getting pet details

--- trunk/petsitter-android-client/src/com/android/petswitch/util/RestClientFactory.java
+++ trunk/petsitter-android-client/src/com/android/petswitch/util/RestClientFactory.java
@@ -8,4 +8,8 @@ public class RestClientFactory {
 	public static RestClient getSearchPetSitterClient(SharedPreferences pref) {
 		return new RestClient (pref, ApplicationConstants.PETSITTER_SEARCH_JSON_URL);
 	}
+	
+	public static RestClient getPetSitterPetDetailClient(SharedPreferences pref) {
+        return new RestClient (pref, ApplicationConstants.PETSITTER_PETDETAIL_URL);
+	}
 }
