f3fcd9a953136ef4c64fc1bd3abccf28bbd64005
nikitha.mamidi@gmail.com <nikitha.mamidi@gmail.com@23e45407-9a27-f170-2efe-fffcf4b299b2>
2012-05-01 22:07:27 +0000

    added url for getting petdetails

--- trunk/petsitter-android-client/src/com/android/petswitch/util/ApplicationConstants.java
+++ trunk/petsitter-android-client/src/com/android/petswitch/util/ApplicationConstants.java
@@ -7,6 +7,9 @@ public class ApplicationConstants {
 	public static String APPLICATION_HOST = "http://10.0.2.2:8080";
 	public static String PETSITTER_SEARCH_JSON_URL = APPLICATION_HOST
 			+ "/petsitter/search/simple.json";
+	
+	public static String PETSITTER_PETDETAIL_URL = APPLICATION_HOST
+			+ "/petsitter/petdetails/simple.json";
 
 	public static String USERNAME = "userName";
 	public static String PASSWORD = "password";
