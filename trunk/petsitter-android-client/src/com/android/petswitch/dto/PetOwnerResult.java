7142b027ea1202d5b1131c9b9cf45c1b3c7213c3
lin.hua.cheng2008@gmail.com <lin.hua.cheng2008@gmail.com@23e45407-9a27-f170-2efe-fffcf4b299b2>
2012-05-01 05:58:17 +0000

    add getter for micro degree

--- trunk/petsitter-android-client/src/com/android/petswitch/dto/PetOwnerResult.java
+++ trunk/petsitter-android-client/src/com/android/petswitch/dto/PetOwnerResult.java
@@ -5,126 +5,126 @@ import java.math.BigInteger;
 
 public class PetOwnerResult {
 
-    private String displayName;
-    private BigInteger id;
-    private String firstName;
-    private String lastName;
-    private String email;
-    private String addressLine1;
-    private String addressLine2;
-    private String city;
-    private String zip;
-    private String country;
-    private String homePhone;
-    private String mobile;
-    private Double averageRating;
-    private String state;
-    private BigDecimal latitude;
-    private BigDecimal longitude;
+	private String displayName;
+	private BigInteger id;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String addressLine1;
+	private String addressLine2;
+	private String city;
+	private String zip;
+	private String country;
+	private String homePhone;
+	private String mobile;
+	private Double averageRating;
+	private String state;
+	private Double latitude;
+	private Double longitude;
 
-    public String getDisplayName() {
-        return displayName;
-    }
+	public String getDisplayName() {
+		return displayName;
+	}
 
-    public void setDisplayName(String displayName) {
-        this.displayName = displayName;
-    }
+	public void setDisplayName(String displayName) {
+		this.displayName = displayName;
+	}
 
-    public BigInteger getId() {
-        return id;
-    }
+	public BigInteger getId() {
+		return id;
+	}
 
-    public void setId(BigInteger id) {
-        this.id = id;
-    }
+	public void setId(BigInteger id) {
+		this.id = id;
+	}
 
-    public String getFirstName() {
-        return firstName;
-    }
+	public String getFirstName() {
+		return firstName;
+	}
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 
-    public String getLastName() {
-        return lastName;
-    }
+	public String getLastName() {
+		return lastName;
+	}
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    public String getAddressLine1() {
-        return addressLine1;
-    }
+	public String getAddressLine1() {
+		return addressLine1;
+	}
 
-    public void setAddressLine1(String addressLine1) {
-        this.addressLine1 = addressLine1;
-    }
+	public void setAddressLine1(String addressLine1) {
+		this.addressLine1 = addressLine1;
+	}
 
-    public String getAddressLine2() {
-        return addressLine2;
-    }
+	public String getAddressLine2() {
+		return addressLine2;
+	}
 
-    public void setAddressLine2(String addressLine2) {
-        this.addressLine2 = addressLine2;
-    }
+	public void setAddressLine2(String addressLine2) {
+		this.addressLine2 = addressLine2;
+	}
 
-    public String getCity() {
-        return city;
-    }
+	public String getCity() {
+		return city;
+	}
 
-    public void setCity(String city) {
-        this.city = city;
-    }
+	public void setCity(String city) {
+		this.city = city;
+	}
 
-    public String getZip() {
-        return zip;
-    }
+	public String getZip() {
+		return zip;
+	}
 
-    public void setZip(String zip) {
-        this.zip = zip;
-    }
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
 
-    public String getCountry() {
-        return country;
-    }
+	public String getCountry() {
+		return country;
+	}
 
-    public void setCountry(String country) {
-        this.country = country;
-    }
+	public void setCountry(String country) {
+		this.country = country;
+	}
 
-    public String getHomePhone() {
-        return homePhone;
-    }
+	public String getHomePhone() {
+		return homePhone;
+	}
 
-    public void setHomePhone(String homePhone) {
-        this.homePhone = homePhone;
-    }
+	public void setHomePhone(String homePhone) {
+		this.homePhone = homePhone;
+	}
 
-    public String getMobile() {
-        return mobile;
-    }
+	public String getMobile() {
+		return mobile;
+	}
 
-    public void setMobile(String mobile) {
-        this.mobile = mobile;
-    }
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
 
-    public Double getAverageRating() {
-        return averageRating;
-    }
+	public Double getAverageRating() {
+		return averageRating;
+	}
 
-    public void setAverageRating(Double averageRating) {
-        this.averageRating = averageRating;
-    }
+	public void setAverageRating(Double averageRating) {
+		this.averageRating = averageRating;
+	}
 
 	public String getState() {
 		return state;
@@ -134,21 +134,35 @@ public class PetOwnerResult {
 		this.state = state;
 	}
 
-	public BigDecimal getLatitude() {
+	public Double getLatitude() {
 		return latitude;
 	}
 
-	public void setLatitude(BigDecimal latitude) {
+	public void setLatitude(Double latitude) {
 		this.latitude = latitude;
 	}
 
-	public BigDecimal getLongitude() {
+	public Double getLongitude() {
 		return longitude;
 	}
 
-	public void setLongitude(BigDecimal longitude) {
+	public void setLongitude(Double longitude) {
 		this.longitude = longitude;
 	}
-    
 
+	public int getLatitudeE6() {
+		int microDegree = 0;
+		if (latitude != null) {
+			microDegree = new Double(latitude * 1000000.0).intValue();
+		}
+		return microDegree;
+	}
+
+	public int getLongitudeE6() {
+		int microDegree = 0;
+		if (longitude != null) {
+			microDegree = new Double(longitude * 1000000.0).intValue();
+		}
+		return microDegree;
+	}
 }
