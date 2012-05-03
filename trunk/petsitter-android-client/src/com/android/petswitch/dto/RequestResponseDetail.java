e275e0d94c8de31df204b68084d74597dba7acae
lin.hua.cheng2008@gmail.com <lin.hua.cheng2008@gmail.com@23e45407-9a27-f170-2efe-fffcf4b299b2>
2012-05-03 07:41:38 +0000

    Updated bean

--- trunk/petsitter-android-client/src/com/android/petswitch/dto/RequestResponseDetail.java
+++ trunk/petsitter-android-client/src/com/android/petswitch/dto/RequestResponseDetail.java
@@ -1,20 +1,28 @@
 package com.android.petswitch.dto;
 
+import java.math.BigInteger;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import com.android.petswitch.util.ApplicationConstants;
 
 public class RequestResponseDetail {
 
+	
 	private String comment;
 	private String status;
 	private String requestorUserName;
 	private String approverUserName;
-	private String requestorAddress;
+	private String requestorAddress;  
 	private String approverAddress; 
 	private String requestStartDate;
 	private String requestEndDate;
 	private String petType;
-	
-	
+    private String requesterPhoneNumber;
+    private String approverPhoneNumber;
+    private BigInteger requestId;
+        
 	public String getComment() {
 		return comment;
 	}
@@ -70,6 +78,53 @@ public class RequestResponseDetail {
 		this.petType = petType;
 	}
 
+
+    public String getRequesterPhoneNumber() {
+        return requesterPhoneNumber;
+    }
+
+    public void setRequesterPhoneNumber(String requesterPhoneNumber) {
+        this.requesterPhoneNumber = requesterPhoneNumber;
+    }
+
+    public String getApproverPhoneNumber() {
+        return approverPhoneNumber;
+    }
+
+    public void setApproverPhoneNumber(String approverPhoneNumber) {
+        this.approverPhoneNumber = approverPhoneNumber;
+    }
+
+    public BigInteger getRequestId() {
+        return requestId;
+    }
+
+    public void setRequestId(BigInteger requestId) {
+        this.requestId = requestId;
+    }
 	
 	
+    private SimpleDateFormat formatter = new SimpleDateFormat (ApplicationConstants.DATE_FORMAT);
+	
+    public Date getRequestStartDateD() {
+    	Date date = new Date();    	
+    	try {
+			date = formatter.parse(requestStartDate);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	return date; 
+	}
+
+    public Date getRequestEndDateD() {
+    	Date date = new Date();    	
+    	try {
+			date = formatter.parse(requestEndDate);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	return date;    			
+	}
 }
