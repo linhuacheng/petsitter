5b6973a16d8c84a529f47b17f2f3eae60059d31d
nikitha.mamidi@gmail.com <nikitha.mamidi@gmail.com@23e45407-9a27-f170-2efe-fffcf4b299b2>
2012-05-04 10:40:03 +0000

    code for accept/reject

--- trunk/petsitter-android-client/src/com/android/petswitch/RequestDetails.java
+++ trunk/petsitter-android-client/src/com/android/petswitch/RequestDetails.java
@@ -1,7 +1,15 @@
 package com.android.petswitch;
 
+import com.android.petswitch.util.ApplicationConstants;
+import com.android.petswitch.util.RequestMethod;
+import com.android.petswitch.util.RestClient;
+import com.android.petswitch.util.RestClientFactory;
+
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -23,6 +31,9 @@ public class RequestDetails extends Activity {
 	private String req_start_date;
 	private String req_end_date;
 	private String message; 
+	private String requestId;
+
+	private String result = "new";
 
 	public void onCreate(Bundle icicle) {
 		super.onCreate(icicle);
@@ -34,6 +45,7 @@ public class RequestDetails extends Activity {
 		req_start_date = getIntent().getStringExtra("reqStartDate");
 		req_end_date = getIntent().getStringExtra("reqEndDate");
 		message = getIntent().getStringExtra("comment");
+		requestId = getIntent().getStringExtra("requestId");
 		
 		
 		petType = (TextView) findViewById(R.id.pet_type);
@@ -41,6 +53,8 @@ public class RequestDetails extends Activity {
 		reqStartDate = (TextView) findViewById(R.id.req_start_date);
 		reqEndDate = (TextView) findViewById(R.id.req_end_date);
 		comment = (TextView) findViewById(R.id.message);
+		accept = (Button) findViewById(R.id.accept);
+		reject = (Button) findViewById(R.id.reject);
 		
 		petType.setText(pet_type);
 		userName.setText(user_name);
@@ -54,14 +68,91 @@ public class RequestDetails extends Activity {
 		}
 		else
 		{
-			accept = (Button) findViewById(R.id.accept);
-			reject = (Button) findViewById(R.id.reject);
-			
-			accept.setVisibility(Button.INVISIBLE);
-			reject.setVisibility(Button.INVISIBLE);
+			accept.setVisibility(Button.GONE);
+			reject.setVisibility(Button.GONE);
 			status = (TextView) findViewById(R.id.status);
 			status.setText(req_status);
 		}
+	
+		accept.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				updateRequest("Accepted");
+				result = "Accepted";
+				req_status = result;
+				accept.setVisibility(Button.GONE);
+				reject.setVisibility(Button.GONE);
+				status = (TextView) findViewById(R.id.status);
+				status.setText(req_status);
+				
+				setResult(RESULT_OK);
+		         finish();
+				
+				
+//				Intent intent = new Intent(RequestDetails.this, RequestList.class);
+//				startActivity(intent);
+			}
+			
+		});
+		
+		
+		reject.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				updateRequest("Rejected");
+				result = "Rejected";
+				req_status = result;
+				accept.setVisibility(Button.GONE);
+				reject.setVisibility(Button.GONE);
+				status = (TextView) findViewById(R.id.status);
+				status.setText(req_status);
+				
+				setResult(RESULT_OK);
+		        finish();
+		         
+//				Intent intent = new Intent(RequestDetails.this, RequestList.class);
+//				startActivity(intent);
+			}
+			
+		});
 		
 	}
+	
+	public void updateRequest(String result)
+	{
+		new DataThread().start();
+		return;
+	}
+	
+	
+	class DataThread extends Thread {
+
+		private static final String INNER_TAG = "getPetDetails";
+
+		public void run() {
+
+			SharedPreferences prefs = getSharedPreferences(
+					ApplicationConstants.USER_PREF, 0);
+			
+			RestClient client = RestClientFactory
+					.getRequestClient(prefs);
+			
+//			Log.i(INNER_TAG, user_name);
+			
+			client.addParam("result", result);
+			client.addParam("requestId", requestId);
+			client.addParam("fromJson", "From Json");
+			
+			try {
+				client.execute(RequestMethod.PUT);
+
+				if (client.getResponseCode() != 200) {
+					// return server error
+					Log.e(INNER_TAG, client.getErrorMessage());
+				}
+				// return valid data
+			} catch (Exception e) {
+				Log.e(INNER_TAG, e.toString());
+			}
+		}
+	}
+		
 }
